96d466f9a2322da70cc40e452fa9c19f68436011
Erik Steiger <rkstgr@gmail.com>
2023-05-07 14:15:59 +0200

    Add headless inference for mdx

--- infer.py
+++ infer.py
@@ -1,6 +1,28 @@
-from mdx import SeperateMDX
+import dataclasses
+import hashlib
+import json
+from dataclasses import dataclass
+from pathlib import Path
+
+from mdx import MDX
+
+PROJECT_ROOT = Path(__file__).parent
+
 
 # main cli entry point
+
+model_params = {
+    "compensate": 1.043,
+    }
+
+
+def get_model_params(model_path) -> dict:
+    model_hash = hashlib.md5(open(model_path, 'rb').read()).hexdigest()
+    with open(PROJECT_ROOT / "models/MDX_NET_Models/model_data/model_data.json", "r") as f:
+        all_model_settings = json.load(f)
+        print(all_model_settings)
+    return all_model_settings[model_hash]
+
 
 def process(input_paths):
     """Start the conversion for all the given mp3 and wav files
@@ -8,11 +30,19 @@ def process(input_paths):
     input_paths: list of paths to the audio files
     """
     for path in input_paths:
-        data = construct_data(path)
-        model = SeperateMDX(model, data)        
+        model_path = PROJECT_ROOT / "models/MDX_NET_Models/Kim_Vocal_1.onnx"
+        model = MDX(model_path, {}, device="cpu")
+        model.separate_file(path, primary_stem=True, secondary_stem=False)
+
 
 def main():
-    pass
+    input_paths = ["/Users/erik/Downloads/Nina Chuba/Lights out sample.mp3"]
+    process(input_paths)
+
+
+def test_mdx():
+    input_paths = ["/Users/erik/Downloads/Nina Chuba/Lights out sample.mp3"]
+    process(input_paths)
 
 
 if __name__ == '__main__':
--- mdx.py
+++ mdx.py
@@ -1,167 +1,277 @@
-import os
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Union, Callable
+
+import librosa
 import torch
 import numpy as np
-from gui_data.constants import MDX_NET_FREQ_CUT, SAVING_STEM
-from lib_v5.mdxnet import MdxnetSet
+
+from lib_v5 import mdxnet
 from lib_v5 import spec_utils
 import onnxruntime as ort
+import soundfile as sf
 
-from separate import prepare_mix, process_secondary_model
+from separate import rerun_mp3
+
+
+def write_audio(stem_path, stem_audio, sample_rate):
+    sf.write(str(stem_path), stem_audio, sample_rate)
+    # encode(stem_path, target_encoding)
+
+
+@NotImplementedError
+@dataclass
+# target_name, lr, optimizer, dim_c, dim_f, dim_t, n_fft, hop_length,
+#                  num_blocks, l, g, k, bn, bias, overlap
+class MDXModelParams:
+    target_name: str
+    lr: float
+    optimizer: str
+    dim_c: int
+    dim_f: int
+    dim_t: int
+    n_fft: int
+    hop_length: int
+    num_blocks: int
+    l: int
+    g: int
+    k: int
+    bn: bool
+    bias: bool
+    overlap: int
+
+
+class KimVocal1Params:
+    n_fft = 7680
+    hop = 1024
+    dim_t = 1024
+    dim_c = 4
+    n_bins = 3841
+    dim_f = 3072
+
+
+def prepare_mix(mix, chunk_set, margin_set, mdx_net_cut=False, sample_rate=44100):
+    mix_path = mix
+    if isinstance(mix, str):
+        if not Path(mix).exists():
+            raise FileNotFoundError(f'File {mix_path} does not exist')
+
+        mix, _ = librosa.load(mix_path, mono=False, sr=sample_rate)
+
+    if not np.any(mix) and mix_path.endswith('.mp3'):
+        mix = rerun_mp3(mix_path)
+
+    if mix.ndim == 1:
+        mix = np.asfortranarray([mix, mix])
+    else:
+        # if [n, 2] -> [2, n]
+        if mix.shape[1] in [1, 2]:
+            mix = np.asfortranarray(mix.T)
+
+    def get_segmented_mix(chunk_set=chunk_set):
+        segmented_mix = {}
+
+        samples = mix.shape[-1]
+        margin = margin_set
+        chunk_size = chunk_set * sample_rate
+        assert not margin == 0, 'margin cannot be zero!'
+
+        if margin > chunk_size:
+            margin = chunk_size
+        if chunk_set == 0 or samples < chunk_size:
+            chunk_size = samples
+
+        counter = -1
+        for skip in range(0, samples, chunk_size):
+            counter += 1
+            s_margin = 0 if counter == 0 else margin
+            end = min(skip + chunk_size + margin, samples)
+            start = skip - s_margin
+            segmented_mix[skip] = mix[:, start:end].copy()
+            if end == samples:
+                break
+
+        return segmented_mix
+
+    segmented_mix = get_segmented_mix()
+    raw_mix = get_segmented_mix(chunk_set=0) if mdx_net_cut else mix
+    return segmented_mix, raw_mix, sample_rate
+
 
 class MDX:
-    def __init__(self, model_path):
+    SAMPLE_RATE = 44100
+
+    def __init__(self, model_path: Union[str, Path], model_params: dict, device: str = 'cuda'):
         self.model_path = model_path
-        # TODO load model into memory
+        self.model_params = self.initialize_model_params()
+        self.device = device
+        self.model_fn = self.initialize_model_fn(self.model_path, self.device)
 
-    def write_to_console(self, text, base_text='\n'):
-        print(f'{base_text}{text}', end='\r')
+        self.chunks = 0
+        self.margin = self.SAMPLE_RATE
 
-    # MAIN METHOD
-    def seperate(self, audio_file_path):
-        audio_file_path = Path(audio_file_path)
-        samplerate = 44100
-          
-        if self.primary_model_name == self.model_basename and self.primary_sources:
-            self.primary_source, self.secondary_source = self.load_cached_sources()
-        else:
-            self.start_inference_console_write()
-
-        if self.model_path.endswith('.ckpt'):
-            model_params = torch.load(self.model_path, map_location=lambda storage, loc: storage)['hyper_parameters']
-            self.dim_c, self.hop = model_params['dim_c'], model_params['hop_length']
-            separator = MdxnetSet.ConvTDFNet(**model_params)
-            self.model_run = separator.load_from_checkpoint(self.model_path).to(self.device).eval()
-        elif self.model_path.endswith('.onnx'):
-            ort_ = ort.InferenceSession(self.model_path, providers=self.run_type)
-            self.model_run = lambda spek:ort_.run(None, {'input': spek.cpu().numpy()})[0]
+    @staticmethod
+    def initialize_model_fn(model_path, device) -> Callable[[torch.Tensor], torch.Tensor]:
+        if model_path.suffix == '.ckpt':
+            model_params = torch.load(str(model_path), map_location=device)['hyper_parameters']
+            # dim_c, hop = model_params['dim_c'], model_params['hop_length']
+            separator = mdxnet.ConvTDFNet(**model_params)
+            return separator.load_from_checkpoint(model_path).to(device).eval()
+        elif model_path.suffix == '.onnx':
+            ort_ = ort.InferenceSession(str(model_path), providers=[
+                'CUDAExecutionProvider' if device == 'cuda' else 'CPUExecutionProvider'])
+            return lambda ort_input: ort_.run(None, {'input': ort_input.cpu().numpy()})[0]
         else:
             raise ValueError('Invalid model format. Please use either .ckpt or .onnx')
 
-        self.initialize_model_settings()
-        self.running_inference_console_write()
-        mdx_net_cut = True if self.primary_stem in MDX_NET_FREQ_CUT else False
-        mix, raw_mix, samplerate = prepare_mix(self.audio_file, self.chunks, self.margin, mdx_net_cut=mdx_net_cut)
-        source = self.demix_base(mix, is_ckpt=self.is_mdx_ckpt)[0]
-        self.write_to_console("DONE", base_text='')            
+    def separate_file(self, file_path, primary_stem=True, secondary_stem=False):
 
-        if self.is_secondary_model_activated:
-            if self.secondary_model:
-                self.secondary_source_primary, self.secondary_source_secondary = process_secondary_model(self.secondary_model, self.process_data, main_process_method=self.process_method)
-        
-        if not self.is_secondary_stem_only:
-            self.write_to_console(f'{SAVING_STEM[0]}{self.primary_stem}{SAVING_STEM[1]}') if not self.is_secondary_model else None
-            primary_stem_path = os.path.join(self.export_path, f'{self.audio_file_base}_({self.primary_stem}).wav')
-            if not isinstance(self.primary_source, np.ndarray):
-                self.primary_source = spec_utils.normalize(source, self.is_normalization).T
-            self.primary_source_map = {self.primary_stem: self.primary_source}
-            self.write_audio(primary_stem_path, self.primary_source, samplerate, self.secondary_source_primary)
+        audio_file_path = Path(file_path)
 
-        if not self.is_primary_stem_only:
-            self.write_to_console(f'{SAVING_STEM[0]}{self.secondary_stem}{SAVING_STEM[1]}') if not self.is_secondary_model else None
-            secondary_stem_path = os.path.join(self.export_path, f'{self.audio_file_base}_({self.secondary_stem}).wav')
-            if not isinstance(self.secondary_source, np.ndarray):
-                raw_mix = self.demix_base(raw_mix, is_match_mix=True)[0] if mdx_net_cut else raw_mix
-                self.secondary_source, raw_mix = spec_utils.normalize_two_stem(source*self.compensate, raw_mix, self.is_normalization)
-            
-                if self.is_invert_spec:
-                    self.secondary_source = spec_utils.invert_stem(raw_mix, self.secondary_source)
-                else:
-                    self.secondary_source = (-self.secondary_source.T+raw_mix.T)
+        primary_stem_label = "vocals"
+        secondary_stem_label = "instrumental"
+        normalize = False
 
-            self.secondary_source_map = {self.secondary_stem: self.secondary_source}
-            self.write_audio(secondary_stem_path, self.secondary_source, samplerate, self.secondary_source_secondary)
+        # mix, raw_mix are dicts
+        mix, raw_mix, samplerate = prepare_mix(str(audio_file_path), self.chunks, self.margin, mdx_net_cut=True,
+                                               sample_rate=self.SAMPLE_RATE)
+        source = self.demix_base(mix, is_ckpt="ckpt" in self.model_path.suffix)[0]
+
+        if primary_stem:
+            # use audio_file_path name
+            primary_stem_path = audio_file_path.with_name(f'{audio_file_path.stem}_({primary_stem_label}).wav')
+            primary_source = spec_utils.normalize(source, normalize).T
+            write_audio(primary_stem_path, primary_source, self.SAMPLE_RATE)
+
+        if secondary_stem:
+            secondary_stem_path = audio_file_path.with_name(f'{audio_file_path.stem}_({secondary_stem_label}).wav')
+            raw_mix = self.demix_base(raw_mix, is_match_mix=True)[0]
+            self.secondary_source, raw_mix = spec_utils.normalize_two_stem(source * self.model_params["compensate"],
+                                                                           raw_mix, False)
+
+            self.secondary_source = (-self.secondary_source.T + raw_mix.T)
+
+            write_audio(secondary_stem_path, self.secondary_source, self.SAMPLE_RATE)
 
         torch.cuda.empty_cache()
-        secondary_sources = {**self.primary_source_map, **self.secondary_source_map}
 
-        self.cache_source(secondary_sources)
-
-        if self.is_secondary_model:
-            return secondary_sources
-
-    def initialize_model_settings(self):
+    def initialize_model_params(self):
         """Initialize the model settings"""
-        self.n_bins = self.n_fft//2+1
-        self.trim = self.n_fft//2
-        self.chunk_size = self.hop * (self.dim_t-1)
-        self.window = torch.hann_window(window_length=self.n_fft, periodic=False).to(self.device)
-        self.freq_pad = torch.zeros([1, self.dim_c, self.n_bins-self.dim_f, self.dim_t]).to(self.device)
-        self.gen_size = self.chunk_size-2*self.trim
+
+        # hardcoded for mdx model "kim vocal 1"
+        n_fft = 7680
+        hop = 1024
+        dim_t = 256
+        dim_c = 4
+        n_bins = 3841
+        dim_f = 3072
+
+        params = {
+            "n_fft": n_fft,
+            "hop": hop,
+            "dim_t": dim_t,
+            "dim_c": dim_c,
+            "dim_f": dim_f,
+            "n_bins": n_fft // 2 + 1,
+            "trim": n_fft // 2,
+            "chunk_size": hop * (dim_t - 1),
+            "window": torch.hann_window(window_length=n_fft, periodic=False),
+            "freq_pad": torch.zeros([1, dim_c, n_bins - dim_f, dim_t]),
+            "gen_size": hop * (dim_t - 1) - 2 * (n_fft // 2),
+        }
+        return params
 
     def initialize_mix(self, mix, is_ckpt=False):
+        gen_size = self.model_params["gen_size"]
+        trim = self.model_params["trim"]
+        chunk_size = self.model_params["chunk_size"]
+
         if is_ckpt:
-            pad = self.gen_size + self.trim - ((mix.shape[-1]) % self.gen_size)
-            mixture = np.concatenate((np.zeros((2, self.trim), dtype='float32'),mix, np.zeros((2, pad), dtype='float32')), 1)
-            num_chunks = mixture.shape[-1] // self.gen_size
-            mix_waves = [mixture[:, i * self.gen_size: i * self.gen_size + self.chunk_size] for i in range(num_chunks)]
+            pad = gen_size + trim - ((mix.shape[-1]) % gen_size)
+            mixture = np.concatenate(
+                (np.zeros((2, trim), dtype='float32'), mix, np.zeros((2, pad), dtype='float32')), 1)
+            num_chunks = mixture.shape[-1] // gen_size
+            mix_waves = [mixture[:, i * gen_size: i * gen_size + chunk_size] for i in range(num_chunks)]
         else:
             mix_waves = []
             n_sample = mix.shape[1]
-            pad = self.gen_size - n_sample%self.gen_size
-            mix_p = np.concatenate((np.zeros((2,self.trim)), mix, np.zeros((2,pad)), np.zeros((2,self.trim))), 1)
+            pad = gen_size - n_sample % gen_size
+            mix_p = np.concatenate((np.zeros((2, trim)), mix, np.zeros((2, pad)), np.zeros((2, trim))), 1)
             i = 0
             while i < n_sample + pad:
-                waves = np.array(mix_p[:, i:i+self.chunk_size])
+                waves = np.array(mix_p[:, i:i + chunk_size])
                 mix_waves.append(waves)
-                i += self.gen_size
-                
+                i += gen_size
+
         mix_waves = torch.tensor(mix_waves, dtype=torch.float32).to(self.device)
 
         return mix_waves, pad
-    
+
     def demix_base(self, mix, is_ckpt=False, is_match_mix=False):
+        trim = self.model_params["trim"]
+        adjust = 1
+        mdx_batch_size = 1
+
         chunked_sources = []
         for slice in mix:
             sources = []
             tar_waves_ = []
             mix_p = mix[slice]
             mix_waves, pad = self.initialize_mix(mix_p, is_ckpt=is_ckpt)
-            mix_waves = mix_waves.split(self.mdx_batch_size)
+            mix_waves = mix_waves.split(mdx_batch_size)
             pad = mix_p.shape[-1] if is_ckpt else -pad
             with torch.no_grad():
                 for mix_wave in mix_waves:
-                    self.running_inference_progress_bar(len(mix)*len(mix_waves), is_match_mix=is_match_mix)
                     tar_waves = self.run_model(mix_wave, is_ckpt=is_ckpt, is_match_mix=is_match_mix)
                     tar_waves_.append(tar_waves)
-                tar_waves_ = np.vstack(tar_waves_)[:, :, self.trim:-self.trim] if is_ckpt else tar_waves_
+                tar_waves_ = np.vstack(tar_waves_)[:, :, trim:-trim] if is_ckpt else tar_waves_
                 tar_waves = np.concatenate(tar_waves_, axis=-1)[:, :pad]
                 start = 0 if slice == 0 else self.margin
                 end = None if slice == list(mix.keys())[::-1][0] or self.margin == 0 else -self.margin
-                sources.append(tar_waves[:,start:end]*(1/self.adjust))
+                sources.append(tar_waves[:, start:end] * (1 / adjust))
             chunked_sources.append(sources)
         sources = np.concatenate(chunked_sources, axis=-1)
-        
+
         return sources
 
     def run_model(self, mix, is_ckpt=False, is_match_mix=False):
-        
-        spek = self.stft(mix.to(self.device))*self.adjust
-        spek[:, :, :3, :] *= 0 
+        adjust = 1
+        trim = self.model_params["trim"]
+
+        spek = self.stft(mix.to(self.device)) * adjust
+        spek[:, :, :3, :] *= 0
 
         if is_match_mix:
             spec_pred = spek.cpu().numpy()
         else:
-            spec_pred = -self.model_run(-spek)*0.5+self.model_run(spek)*0.5 if self.is_denoise else self.model_run(spek)
+            spec_pred = -self.model_fn(-spek) * 0.5 + self.model_fn(spek)
 
         if is_ckpt:
             return self.istft(spec_pred).cpu().detach().numpy()
-        else: 
-            return self.istft(torch.tensor(spec_pred).to(self.device)).to("cpu")[:,:,self.trim:-self.trim].transpose(0,1).reshape(2, -1).numpy()
-    
-    def stft(self, x):
-        x = x.reshape([-1, self.chunk_size])
-        x = torch.stft(x, n_fft=self.n_fft, hop_length=self.hop, window=self.window, center=True,return_complex=True)
-        x=torch.view_as_real(x)
-        x = x.permute([0,3,1,2])
-        x = x.reshape([-1,2,2,self.n_bins,self.dim_t]).reshape([-1,self.dim_c,self.n_bins,self.dim_t])
-        return x[:,:,:self.dim_f]
+        else:
+            return self.istft(torch.tensor(spec_pred).to(self.device)).to("cpu")[:, :, trim:-trim].transpose(
+                0, 1).reshape(2, -1).numpy()
+
+    def stft(self, x: torch.Tensor):
+        params = self.model_params
+        chunk_size = params["chunk_size"]
+        x = x.reshape([-1, chunk_size])
+        x = torch.stft(x, n_fft=params["n_fft"], hop_length=params["hop"], window=params["window"].to(x.device),
+                       center=True, return_complex=True)
+        x = torch.view_as_real(x)
+        x = x.permute([0, 3, 1, 2])
+        x = x.reshape([-1, 2, 2, params["n_bins"], params["dim_t"]]) \
+            .reshape([-1, params["dim_c"], params["n_bins"], params["dim_t"]])
+        return x[:, :, :params["dim_f"]]
 
     def istft(self, x, freq_pad=None):
-        freq_pad = self.freq_pad.repeat([x.shape[0],1,1,1]) if freq_pad is None else freq_pad
+        params = self.model_params
+        freq_pad = params["freq_pad"].repeat([x.shape[0], 1, 1, 1]) if freq_pad is None else freq_pad
         x = torch.cat([x, freq_pad], -2)
-        x = x.reshape([-1,2,2,self.n_bins,self.dim_t]).reshape([-1,2,self.n_bins,self.dim_t])
-        x = x.permute([0,2,3,1])
-        x=x.contiguous()
-        x=torch.view_as_complex(x)
-        x = torch.istft(x, n_fft=self.n_fft, hop_length=self.hop, window=self.window, center=True)
-        return x.reshape([-1,2,self.chunk_size])
+        x = x.reshape([-1, 2, 2, params["n_bins"], params["dim_t"]]).reshape([-1, 2, params["n_bins"], params["dim_t"]])
+        x = x.permute([0, 2, 3, 1])
+        x = x.contiguous()
+        x = torch.view_as_complex(x)
+        x = torch.istft(x, n_fft=params["n_fft"], hop_length=params["hop"], window=params["window"].to(x.device),
+                        center=True)
+        return x.reshape([-1, 2, params["chunk_size"]])
